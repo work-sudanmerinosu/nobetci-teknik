215f4b17cfdd46f230cb88a2973fa1486868a3cd
Muhammed Salih Gümüş <sudanmerinosu@Muhammeds-Laptop.local>
2023-02-28 01:34:53 +0300

    [Salih] clean up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+	app: {
+		head: {
+			title: 'Nöbetçi Teknik',
+			htmlAttrs: {
+				lang: 'tr',
+			},
+		},
+	},
+});
