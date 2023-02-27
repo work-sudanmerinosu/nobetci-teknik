8185e9cce12ea183511433c2f698289876801329
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2023-02-28 01:42:12 +0300

    [Salih] init tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	css: ['~/assets/css/main.css'],
 	app: {
 		head: {
 			title: 'Nöbetçi Teknik',
 			htmlAttrs: {
 				lang: 'tr',
 			},
+		},
+	},
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
 		},
 	},
 });
