36d42d490ca83e32038a7abd33398212430dc940
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2023-03-14 21:09:20 +0300

    [Salih] add gallery template

---
+++ components/Gallery.vue
@@ -0,0 +1,66 @@
+<template>
+	<section class="text-gray-600 body-font">
+		<div class="container px-5 py-24 mx-auto flex flex-wrap">
+			<div class="flex w-full mb-20 flex-wrap">
+				<h1
+					class="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4"
+				>
+					Nöbetçi Teknik Servis
+				</h1>
+				<p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">
+					Nöbetçi Teknik Servisimiz Antalya'nın her yerinde evinizin ve iş
+					yerinizin her türlü teknik ihtiyacında 7/24 kesintisiz hizmet
+					vermektedir..
+				</p>
+			</div>
+			<div class="flex flex-wrap md:-m-2 -m-1">
+				<div class="flex flex-wrap w-1/2">
+					<div class="md:p-2 p-1 w-1/2">
+						<img
+							alt="gallery"
+							class="w-full object-cover h-full object-center block"
+							src="https://dummyimage.com/500x300"
+						/>
+					</div>
+					<div class="md:p-2 p-1 w-1/2">
+						<img
+							alt="gallery"
+							class="w-full object-cover h-full object-center block"
+							src="https://dummyimage.com/501x301"
+						/>
+					</div>
+					<div class="md:p-2 p-1 w-full">
+						<img
+							alt="gallery"
+							class="w-full h-full object-cover object-center block"
+							src="https://dummyimage.com/600x360"
+						/>
+					</div>
+				</div>
+				<div class="flex flex-wrap w-1/2">
+					<div class="md:p-2 p-1 w-full">
+						<img
+							alt="gallery"
+							class="w-full h-full object-cover object-center block"
+							src="https://dummyimage.com/601x361"
+						/>
+					</div>
+					<div class="md:p-2 p-1 w-1/2">
+						<img
+							alt="gallery"
+							class="w-full object-cover h-full object-center block"
+							src="https://dummyimage.com/502x302"
+						/>
+					</div>
+					<div class="md:p-2 p-1 w-1/2">
+						<img
+							alt="gallery"
+							class="w-full object-cover h-full object-center block"
+							src="https://dummyimage.com/503x303"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,5 @@
-<template></template>
+<template>
+	<Gallery />
+	<!-- <Features />
+	<LastJobs /> -->
+</template>
