c3b6fcea93c2c734c8ccf34df19920234d7cbf59
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2023-03-14 20:50:35 +0300

    [Salih] add navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+	<header class="text-gray-600 body-font">
+		<div
+			class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
+		>
+			<a
+				class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+			>
+				<nuxt-img src="./images/logo.png" class="h-16" />
+			</a>
+			<nav
+				class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center"
+			>
+				<a class="mr-5 hover:text-gray-900" href="tel:+90-542-802-46-30">
+					<Icon name="ic:baseline-local-phone" />
+					542-802-46-30</a
+				>
+				<a class="mr-5 hover:text-gray-900" href="tel:+90-538-239-39-96">
+					<Icon name="ic:baseline-local-phone" />
+					538-239-39-96</a
+				>
+			</nav>
+			<a
+				class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
+				href="https://goo.gl/maps/m9hsZ7g9AesGSGr39"
+				target="_blank"
+			>
+				<Icon name="ic:baseline-location-on" />
+
+				Ahatlı mahallesi Ulusoy Cad. 96/A Kepez/Antalya
+			</a>
+		</div>
+	</header>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,1 @@
-<template>
-	<h1 class="text-3xl font-bold underline">Hello world!</h1>
-</template>
+<template></template>
