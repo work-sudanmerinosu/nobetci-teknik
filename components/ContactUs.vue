2c1d4e52ccc718cf483839318d5b0554b106f615
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2023-04-08 02:17:13 +0300

    [Salih] complate contact us

--- components/ContactUs.vue
+++ components/ContactUs.vue
@@ -49,55 +49,58 @@
 					</div>
 				</div>
 			</div>
-			<div
+			<form
+				method="get"
+				action="mailto:nobetciteknikservis07@gmail.com"
 				class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
 			>
 				<h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
-					Feedback
+					Bizimle İletişime Geçin
 				</h2>
 				<p class="leading-relaxed mb-5 text-gray-600">
-					Post-ironic portland shabby chic echo park, banjo fashion axe
+					Daha iyi bir hizmet için istek ve önerilerinizi yazabilirsiniz.
 				</p>
 				<div class="relative mb-4">
-					<label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+					<label for="name" class="leading-7 text-sm text-gray-600"
+						>Adınız</label
+					>
 					<input
 						type="text"
 						id="name"
 						name="name"
+						required
 						class="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
 					/>
 				</div>
 				<div class="relative mb-4">
-					<label for="email" class="leading-7 text-sm text-gray-600"
-						>Email</label
+					<label for="subject" class="leading-7 text-sm text-gray-600"
+						>Konu</label
 					>
 					<input
-						type="email"
-						id="email"
-						name="email"
+						type="subject"
+						id="subject"
+						name="subject"
+						required
 						class="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
 					/>
 				</div>
 				<div class="relative mb-4">
 					<label for="message" class="leading-7 text-sm text-gray-600"
-						>Message</label
+						>Mesaj</label
 					>
 					<textarea
 						id="message"
-						name="message"
+						name="body"
+						required
 						class="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
 					></textarea>
 				</div>
 				<button
 					class="text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg"
 				>
-					Button
+					Gönder
 				</button>
-				<p class="text-xs text-gray-500 mt-3">
-					Chicharrones blog helvetica normcore iceland tousled brook viral
-					artisan.
-				</p>
-			</div>
+			</form>
 		</div>
 	</section>
 </template>
